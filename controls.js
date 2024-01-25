3843ef849e3b759f74fca7e8f4e54d939f4fab71
Muhammad Umer <mhadisb1212@gmail.com>
2024-01-25 21:48:44 +0500

    Car Movement done

---
+++ controls.js
@@ -0,0 +1,45 @@
+export class Controls {
+  constructor() {
+    this.forward = false;
+    this.left = false;
+    this.right = false;
+    this.reverse = false;
+
+    this.#addKeyBoardListners();
+  }
+
+  #addKeyBoardListners() {
+    document.onkeydown = (e) => {
+      switch (e.key) {
+        case "ArrowLeft":
+          this.left = true;
+          break;
+        case "ArrowRight":
+          this.right = true;
+          break;
+        case "ArrowUp":
+          this.forward = true;
+          break;
+        case "ArrowDown":
+          this.reverse = true;
+          break;
+      }
+    };
+    document.onkeyup = (e) => {
+      switch (e.key) {
+        case "ArrowLeft":
+          this.left = false;
+          break;
+        case "ArrowRight":
+          this.right = false;
+          break;
+        case "ArrowUp":
+          this.forward = false;
+          break;
+        case "ArrowDown":
+          this.reverse = false;
+          break;
+      }
+    };
+  }
+}
